0ad1113d1944e71b358a7c7fcc0f8d40dc2e9c1c
Daniel Checchia <daniel@checchia.net>
2020-05-07 20:07:49 -0300

    Initial

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name='smdb',
   packages = ['smdb'], # this must be the same as the name above
   version='0.0.1',
-  description='A sample MongoDB backup',
+  description='A simple MongoDB backup',
   author='Checchia.NET - IT Solutions',
   author_email='dono@checchia.net',
   license='GPLv3',
