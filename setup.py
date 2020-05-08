a83b7ee4dbb2f5e2746334b3dbc80f64bcb710b8
Daniel Checchia <daniel@checchia.net>
2020-05-07 22:05:44 -0300

    Initial

--- setup.py
+++ setup.py
@@ -5,21 +5,32 @@
 #  \___|_||_\___\__\__|_||_|_\__,_(_)_|\_|___| |_|
 #
 # Created by Daniel Checchia on 23/04/2020 at 09:58
-# Copyright (c) 2020 - Checchia.NET (https://Checchia.NET)
+# CopyLeft (c) 2020 - GPLv3 - Checchia.NET (https://Checchia.NET)
 #
-import pathlib
-script_dir = pathlib.Path(__file__).parent.resolve()
+import sys
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+if sys.version < '3.6':
+    print("ERROR: python version 3.6 or higher is required")
+    sys.exit(1)
+
 setup(
   name='SimpleMongoMackup',
-  packages = ['SimpleMongoMackup'], # this must be the same as the name above
+  packages = find_packages(),
   version='0.0.1',
   description='A simple MongoDB backup',
   author='Checchia.NET - IT Solutions',
   author_email='dono@checchia.net',
   license='GPLv3',
+  install_requires = ["boto3 >= 1.13"],
   url='https://github.com/checchia/SimpleMongoMackup', # The project's main homepage.
   keywords = ['MongoDB', 'backup', 'backup Rotate'], # arbitrary keywords
-  classifiers = [],
+  classifiers = [
+             "Development Status :: 5 - Production",
+             "License :: OSI Approved :: GNU General Public License v3",
+             "Operating System :: POSIX :: Linux",
+             "Natural Language :: Portuguese (Brazilian)"
+             "Programming Language :: Python :: 3.6",
+             "Topic :: Database",
+             ],
 )
